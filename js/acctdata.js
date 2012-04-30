b4b4683d47434fffe22792f3324b47ba553ce0c1
Nathan E. Fairchild <natefair@tellme.com>
2012-04-29 18:53:57 -0700

    Adding AccountData.util module. Working to remove global variables and replace with dependency injection. Reworking to allow TransactionList module to manipulate the transaction list and the transaction details pages.

--- js/acctdata.js
+++ js/acctdata.js
@@ -1,9 +1,10 @@
 var AccountData = AccountData || {};
-var g_acct_number = 1;
 
 // Use this BaseUrl attribute to change the global location of the data
-AccountData.BaseUrl = location.protocol + '//' +
-    location.hostname + '/247demo/transactions/data/acctdata';
+AccountData.BaseUrl = 'data/acctdata';
+//AccountData.BaseUrl = location.protocol + '//' +
+//    location.hostname + '/~natefair/247demo/data/acctdata';
+//    location.hostname + '/demo-mm-transactions/data/acctdata';
 
 /*
  * Account Data Loader
@@ -25,41 +26,90 @@ AccountData.BaseUrl = location.protocol + '//' +
  */
 AccountData.account = (function($) {
     var data = null;
+    var _active_acct_number = null;
+    var _active_cc_number = null;
 
-    var makeAccountUrl = function(acct_number) {
-        return AccountData.BaseUrl + '/' + acct_number + '.json';
+    var mc_img = '<img src="img/mastercard.png" height="28px" style="padding-top: 2px;"/>';
+
+
+    var makeAccountUrl = function() {
+        return AccountData.BaseUrl + '/' + _active_acct_number + '.json';
     };
 
-    var listCards = function(div, data, selected_cc_number) {
-        var img = '<img src="http://www.creditcards.com/images/Bank-Logos-Mastercard.gif" />';
+
+    var select_element = function(selected, disabled) {
+        var opt = disabled ? ' disabled="disabled"' : '';
+        return $('<select name="card-choice" id="card-choice"' +
+                 ' data-mini="true"' + opt + '></select>');
+    };
+
+
+    var select_option = function(cc_number, selected) {
+        var last_4 = cc_number.substr(-4);
+        var extra  = $.inArray(cc_number, [selected, _active_cc_number]) < 0 ?
+                     '' : ' selected="selected"';
+        var option = '<option value="' + cc_number + '"' + extra +
+                     '>...' + last_4 + '</option>';
+        return $(option);
+    };
+
+
+    var listCards = function(div, disabled) {
         var div = $('#' + div);
         div.empty();
-        $(img).appendTo(div);
-        var sel = $('<select name="card-choice" id="card-choice" data-mini="true"' + 
-            (selected_cc_number? 'disabled' : '') + '></select>').appendTo(div);
-        for (var i = 0; i < data.dest_accounts.length; i += 1) {
-            var cc_num = data.dest_accounts[i].number;
-            var last_4 = cc_num.substr(-4);
-            $('<option value="' + cc_num + '"' + (cc_num == selected_cc_number? 'selected' : '') + 
-                '>...' + last_4 + '</option>').appendTo(sel)
-        }
-        sel.selectmenu();
+
+        var selected_cc_number = AccountData.account.active_cc_number();
+
+        // Add the CC icon
+        $(mc_img).appendTo(div);
+
+        // Build the CC dropdown list
+        var dropdown = select_element(selected_cc_number, disabled).appendTo(div);
+        $(data.dest_accounts).each( function(i, acct) {
+            select_option(acct.number, selected_cc_number).appendTo(dropdown);
+        });
+        dropdown.selectmenu();
+
+        // Setup the default change handler to record active CC number
+        dropdown.on('change', function() {
+            _active_cc_number = dropdown[0].value;
+        });
+        // Call the change handler to update the active CC number
+        dropdown.change();
+
+        return dropdown;
     };
 
     // API bits
     return {
-        initDropdown: function(div, acct_number, selected_cc_number, callback) {
-            var url = makeAccountUrl(acct_number);
-            $.getJSON(url, function(results) {
-                data = results;
-                listCards(div, data, selected_cc_number);
+        init: function(acct_number) {
+            _active_acct_number = acct_number;
+        },
+        initDropdown: function(div, disabled, callback) {
+            var url = makeAccountUrl();
+            if (data === null) {
+                $.getJSON(url, function(results) {
+                    data = results;
+                    var dropdown = listCards(div, disabled);
+                    if (typeof callback === 'function') {
+                        callback(dropdown, data);
+                    }
+                });
+            } else {
+                var dropdown = listCards(div, disabled);
                 if (typeof callback === 'function') {
-                    callback($('#card-choice'), data);
+                    callback(dropdown, data);
                 }
-            });
+            }
         },
         getData: function() {
             return data;
+        },
+        active_acct_number: function() {
+            return _active_acct_number;
+        },
+        active_cc_number: function() {
+            return _active_cc_number;
         }
     };
 })(jQuery);
@@ -85,50 +135,59 @@ AccountData.account = (function($) {
  */
 AccountData.transactions = (function($) {
     var data = null;
+    var active_cc_number = null;
 
     // Return the URL for fetching the CC transaction information
-    var makeCCNumberUrl = function(cc_number) {
-        return AccountData.BaseUrl + '/transactions-' + cc_number + '.json';
+    var makeCCNumberUrl = function() {
+        return AccountData.BaseUrl + '/transactions-' +
+               active_cc_number + '.json';
     };
 
     // Return the HREF for displaying transaction details
-    var transactionShowHref = function(cc_number, transaction) {
-        return '#transaction-detail?cc_number=' + cc_number + 
+    var transactionShowHref = function(idx, transaction) {
+        var page = transaction.is_payment() ? '#payment-detail'
+                                            : '#transaction-detail';
+        return page + '?index=' + idx +
+               '&cc_number=' + AccountData.account.active_cc_number() + 
                '&transaction_id=' + transaction.id;
     };
 
     // Return the HTML for the Date portion of the list item
     var transactionCal = function(transaction) {
         return '<div class="list-cal">' +
-               '<div class="cal-top">' + transaction.dow +
-               ', ' + transaction.month + '</div>' +
+               '<div class="cal-top">' + transaction.dow_month() + '</div>' +
                '<div class="cal-bot">' + transaction.day + '</div>' +
                '</div>';
     };
 
     // Return a short description in 2 divs for the charge list
     var transactionShort = function(transaction) {
-        return transactionCal(transaction) +
-            '<div class="list-partial">' + 
-            '<div class="part-name">' + transaction.name + '</div>' +
-            '<div class="part-addr">' + transaction.city + ', ' +
-            transaction.state + '</div></div>' +
-            '<div class="list-amount">' + transaction.amount + '</div>';
+        var div = transactionCal(transaction) + '<div class="list-partial">';
+        if (transaction.is_payment()) { 
+            div += '<div class="part-payment">' + transaction.name + '</div>';
+        } else {
+            div += '<div class="part-name">' + transaction.name + '</div>';
+            div += '<div class="part-addr">' + transaction.location_short() +
+                   '</div>';
+        }
+        div += '</div>';
+        div += '<div class="list-amount">' + transaction.amount + '</div>';
+        return div;
     };
 
     // Display the list of transactions in the charges-list
-    var displayTransactions = function(cc_number, data) {
+    var displayTransactions = function(data) {
+        var list = $("#charges-list");
+        list.empty();
+        var items = [];
+        for (var i = 0; i < data.transactions.length; i += 1) {
+            var transaction = data.transactions[i];
+            var show = transactionShort(transaction);
+            var href = transactionShowHref(i, transaction);
+            items.push('<li><a data-url="' + href + '" href="' + href + '">' + show + '</a></li>');
+        }
+        $(items.join('')).appendTo(list);
         if (data.transactions.length > 0) {
-            var list = $("#charges-list");
-            list.empty();
-            var items = [];
-            for (var i = 0; i < data.transactions.length; i += 1) {
-                var transaction = data.transactions[i];
-                var href = transactionShowHref(cc_number, transaction);
-                var show = transactionShort(transaction);
-                items.push('<li><a data-url="' + href + '" href="' + href + '">' + show + '</a></li>');
-            }
-            $(items.join('')).appendTo(list);
             list.listview('refresh');
         }
     };
@@ -136,18 +195,22 @@ AccountData.transactions = (function($) {
     // Display a specific transaction in the charge-detail 
     var displayTransactionHelper = function(transaction) {
         if (transaction != null) {
-            var city = transaction.city + ", " + 
-                transaction.state + " " + transaction.zipcode;
-            var mlocation = transaction.address + ", " + city;        
-            $("#amount").html(transaction.amount);
-            $("#datetime").html(transaction.dow + ", " + transaction.month + 
-                " " + transaction.day + ", " + transaction.year + " " + 
-                transaction.time + " " + transaction.zone);
-            $("#merchant").html(transaction.name);
-            $("#mlocation").html(transaction.address + "<br>" + city);
-            $("#map").attr("src", "https://maps.googleapis.com/maps/api/staticmap?center=" + 
-                encodeURIComponent(mlocation) +  "&zoom=14&size=288x200&markers=" + 
-                encodeURIComponent(mlocation) + "&sensor=false");
+            var city = transaction.location();
+            if (transaction.is_payment()) {
+                $('#payment-amount').html(transaction.payment());
+                $('#payment-datetime').html(transaction.full_date());
+            } else {
+                $('#amount').html(transaction.amount);
+                $('#datetime').html(transaction.full_date());
+                $('#merchant').html(transaction.merchant);
+                $('#mlocation').html(transaction.address + '<br>' + city);
+                $('#map').attr('src',
+                    'https://maps.googleapis.com/maps/api/staticmap?center=' + 
+                    encodeURIComponent(transaction.full_address()) +
+                    '&zoom=14&size=288x200&markers=' + 
+                    encodeURIComponent(transaction.full_address()) +
+                    '&sensor=false');
+            }
 /*
             $('#map-canvas').gmap({
                 zoom: 15,
@@ -168,24 +231,50 @@ AccountData.transactions = (function($) {
     };
 
     var getData = function() {
-        return data;
+        if (active_cc_number) {
+            return data[active_cc_number];
+        }
+        return { transactions: [] };
+    };
+
+    var show_transactions = function(data, callback) {
+        if (location.hash === '#recent-transactions') {
+            displayTransactions(data);
+        }
+        if (typeof callback === 'function') {
+            callback(data);
+        }
+    };
+
+    var load_cc_data = function(cc_number, callback) {
+        var url = makeCCNumberUrl(cc_number);
+        $.getJSON(url, function(results) {
+            var list = results.transactions;
+            data[cc_number] = {};
+            data[cc_number].transactions = list.map(function(obj) {
+              return new Transaction(obj);
+            });
+            show_transactions(data[cc_number], callback);
+        });
     };
 
     return {
         display: function(cc_number, callback) {
-            var url = makeCCNumberUrl(cc_number);
-            $.getJSON(url, function(results) {
-                data = results;
-                displayTransactions(cc_number, data);
-                if (typeof callback === 'function') {
-                    callback(data);
-                }
-            });
+            active_cc_number = cc_number
+            data = data || {};
+            if (data[active_cc_number] === undefined) {
+                load_cc_data(cc_number, callback);
+            } else {
+                show_transactions(data[cc_number], callback);
+            }
         },
+        displayTransactions: displayTransactions,
+        displayTransactionHelper: displayTransactionHelper,
+/*
         displayTransaction: function(cc_number, transaction_id, callback) {
             var url = makeCCNumberUrl(cc_number);
             $.getJSON(url, function(results) {
-                for (var i = 0; results.transactions.length > 0 && i < results.transactions.length; i += 1) {
+                for (var i = 0; i < results.transactions.length; i += 1) {
                     var transaction = results.transactions[i];
                     if (transaction_id == transaction.id) {
                         displayTransactionHelper(transaction);
@@ -197,6 +286,100 @@ AccountData.transactions = (function($) {
                 }
             });
         },
+*/
         getData: getData
      };
+})(jQuery);
+
+
+// ----------------------------------------------------------------------------
+// Module: AccountData.utils
+//
+// AccountData.utils.timestamp_to_object(millis)
+// AccountData.utils.transaction_date(transaction)
+// AccountData.utils.transaction_date_object(transaction)
+//
+AccountData.utils = (function($) {
+  var dows = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+  var mons = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
+              'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+
+  // Return printable 2 digit time value for hours, minutes and seconds
+  var two_digit_string = function(num) {
+    return (num < 10) ? '0' + num : '' + num;
+  };
+
+  // Convert milliseconds into displayable date attributes
+  //
+  //  var millis = $.now();
+  //  var dt_obj = AccountData.utils.timestamp_to_object(millis);
+  //
+  // @param timestamp - integer, time in millis
+  // @return object - day, dow, month, year attributes
+  //
+  var timestamp_to_object = function(timestamp) {
+    var date = new Date(timestamp);
+    var hour = two_digit_string(date.getHours());
+    var mins = two_digit_string(date.getMinutes());
+    var secs = two_digit_string(date.getSeconds());
+    var time = [hour, mins, secs].join(':');
+    return {
+      day:     date.getDate(),
+      dow:     dows[date.getDay()],
+      month:   mons[date.getMonth()],
+      year:    date.getYear() + 1900,
+      hhmmss:  time,
+      hours:   hour,
+      minutes: mins,
+      seconds: secs
+    };
+  };
+
+  // Convert 'daysago' or [dow, month, day, year, time, zone] into a
+  // timestamp (in milliseconds). Returns -1 if timestamp cannot be
+  // created.  Updates transaction object with 'timestamp' attribute.
+  //
+  // @param transaction - a transaction object from JSON
+  // @return timestamp in milliseconds
+  //
+  var transaction_date = function(transaction) {
+    var day_millis = 24 * 60 * 60 * 1000;
+    if (transaction['timestamp'] !== undefined) {
+      return transaction.timestamp;
+    }
+    if (transaction['daysago'] !== undefined) {
+      var now = new Date();
+      var hrs = transaction.time.match(/\d+/g).map(function(a) {return a - 0});
+      now.setHours(hrs[0], hrs[1]);
+      var xtm = new Date(now.getTime() - transaction.daysago * day_millis);
+      transaction.timestamp = xtm.getTime();
+      return transaction.timestamp;
+    }
+    if (transaction['dow'] !== undefined) {
+      transaction.timestamp = Date.parse(
+        transaction.dow + ' ' +
+        transaction.month + ' ' +
+        transaction.day + ' ' +
+        transaction.year + ' ' +
+        transaction.time + ' ' +
+        transaction.zone);
+      return transaction.timestamp;
+    }
+    return -1; // Error in transaction object
+  };
+
+  // Return a date object for the given transaction
+  //
+  // @param transaction - a transaction object from JSON
+  // @return object - day, dow, month, year attributes
+  //
+  var transaction_date_object = function(transaction) {
+    return timestamp_to_object(transaction_date(transaction));
+  };
+
+  return {
+    timestamp_to_object: timestamp_to_object,
+    transaction_date: transaction_date,
+    transaction_date_object: transaction_date_object
+  };
 })(jQuery);
